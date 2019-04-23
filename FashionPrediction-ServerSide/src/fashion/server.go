b43134c948c24b0a1dfff63d59854826f7a51c14
Yueqiao Zhang <yueqiaozhang79@gmail.com>
2019-04-22 23:10:57 -0700

    Added /POST prediction API and get prediction result back

--- FashionPrediction-ServerSide/src/fashion/server.go
+++ FashionPrediction-ServerSide/src/fashion/server.go
@@ -9,6 +9,15 @@ import (
 	"github.com/codegangsta/negroni"
 	"github.com/gorilla/mux"
 	"github.com/unrolled/render"
+	"log"
+	"io/ioutil"
+	"io"
+	"github.com/watson-developer-cloud/go-sdk/visualrecognitionv3"
+	"os"
+	"github.com/IBM/go-sdk-core/core"
+	"encoding/json"
+    "fmt"
+    "strings"
 )
 
 // MongoDB Config
@@ -32,31 +41,107 @@ func NewServer() *negroni.Negroni {
 
 // API Routes
 func initRoutes(mx *mux.Router, formatter *render.Render) {
-	mx.HandleFunc("/v1/prediction/{raceid}", predictionHandler(formatter)).Methods("GET")
+	mx.HandleFunc("/v1/prediction", predictionHandler(formatter)).Methods("POST")
 
 }
 
 // API Prediction Handler
 func predictionHandler(formatter *render.Render) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		params := mux.Vars(req)
-		var raceid string = params["raceid"]
-
-		// 1. make request to Waston prediction model
-		// Wastion studio model id endpoint with auth token
-		resp, err := http.Get("")
+	    /**
+			Get Post body
+		**/        
+        body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
 			log.Fatalln(err)
 		}
+		fmt.Println(body)
 
-		// 2. get Waston prediction 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatalln(err)
+		var clothUrl Clothes
+		json.Unmarshal(body, &clothUrl)
+	    /**
+			Connect to Waston
+		**/  
+		visualRecognition, visualRecognitionErr := visualrecognitionv3.NewVisualRecognitionV3(&visualrecognitionv3.VisualRecognitionV3Options{
+	  		Version: "2018-03-19",
+			IAMApiKey: "54qjyFj9ESmM9rKUMnJk0uL2EEs26arlX9ukgdhVoROA",
+		})
+
+		if visualRecognitionErr != nil {
+		   //panic(visualRecognitionErr)
+			fmt.Println("visualRecognitionErr")
 		}
-		
-		formatter.JSON(w, http.StatusOK, raceid)
+		/**
+			Get image from frontend
+		**/
+		//fileUrl := "https://tineye.com/images/widgets/mona.jpg"
+
+	    if err := DownloadFile("test.jpg", clothUrl.ClothesUrl); err != nil {
+	        fmt.Println("DownloadFile")
+	    }
+		fmt.Println("DownloadFile-Success")
+
+		imageFile, imageFileErr := os.Open("test.jpg")
+
+		if imageFileErr != nil {
+			fmt.Println("image file error")
+		}
+		defer imageFile.Close()
+		fmt.Println("Open image-Success")
+
+		  response, responseErr := visualRecognition.Classify(
+		    &visualrecognitionv3.ClassifyOptions{
+		      ImagesFile: imageFile,
+		      Threshold: core.Float32Ptr(0.0),
+		      ClassifierIds: []string{"modelx1_1561818531"},
+		    },
+		  )
+		  fmt.Println("response-Success")
+
+		  if responseErr != nil {
+		  	fmt.Println("response file error")
+		    //panic(responseErr)
+		  }
+
+		  result := visualRecognition.GetClassifyResult(response)
+		  b, _ := json.MarshalIndent(result, "", "  ")
+		  s := strings.Split(string(b), ":")
+		  likeBody := s[len(s)-1]
+		  /**
+		  	 Get prediction like number
+		  **/
+		  likeResult := strings.Split(string(likeBody), "}")[0]
+		  likeResponse := strings.TrimSpace(likeResult)
+		  fmt.Println("likeResponse", likeResponse)
+
+		  var returnResponse Predict
+		  returnResponse.Class = "like"
+		  returnResponse.Score = likeResponse
+	
+		formatter.JSON(w, http.StatusOK, returnResponse)
 	}
+}
+
+func DownloadFile(filepath string, url string) error {
+
+    // Get the data
+    resp, err := http.Get(url)
+    if err != nil {
+        //return err
+        fmt.Println("Get the data")
+    }
+    defer resp.Body.Close()
+
+    // Create the file
+    out, err := os.Create(filepath)
+    if err != nil {
+        fmt.Println("Create the file")
+    }
+    defer out.Close()
+
+    // Write the body to file
+    _, err = io.Copy(out, resp.Body)
+    return err
 }
 
 
--- FashionPrediction-ServerSide/src/fashion/types.go
+++ FashionPrediction-ServerSide/src/fashion/types.go
@@ -1,3 +1,33 @@
 package main
 
 
+type Predict struct {
+	Class	    string   `json:"class"` 
+	Score		string   `json:"score"` 
+}
+
+type Result struct {
+	CustomClass		int  `json:"custom_classes"` 
+	ProcessImage	int	 `json:"images_processed"` 
+	Images			Images `json:"images"`
+}
+
+type Images struct {
+	ImageName		string	`json:"image"`
+	Classify		Classify	`json:"classifiers"`
+}
+
+type Classify struct {
+	Name           string   `json:"name"`
+	Id             string   `json:"classifier_id"`
+	ClassResult    Predict  `json:"classes"`
+}
+
+type ClassResult struct {
+	Class	    string   `json:"class"` 
+	Score		float64   `json:"score"`      
+}
+
+type Clothes struct {
+	ClothesUrl		string  `json:"clothUrl"`
+}
